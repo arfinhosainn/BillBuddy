7fe3d531401b5be2cdc1d960a666f135b99d0ccb
Arfin Hosain <developerarfin@gmail.com>
2023-03-15 18:52:20 +0600

    Add unit tests for PaymentRepository

--- app/src/main/java/com/example/billbuddy/data/local/repository/PaymentRepositoryImpl.kt
+++ app/src/main/java/com/example/billbuddy/data/local/repository/PaymentRepositoryImpl.kt
@@ -1,16 +1,12 @@
 package com.example.billbuddy.data.local.repository
 
-import android.util.Log
-import androidx.compose.runtime.collectAsState
 import com.example.billbuddy.data.local.PaymentDao
 import com.example.billbuddy.data.local.model.InvalidPaymentException
 import com.example.billbuddy.data.local.model.Payment
 import com.example.billbuddy.domain.repository.PaymentRepository
 import com.example.billbuddy.util.Resource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
 import java.io.IOException
 import javax.inject.Inject
 
@@ -23,9 +19,9 @@ class PaymentRepositoryImpl @Inject constructor(
         if (payments.paymentTitle.isBlank()) {
             throw InvalidPaymentException("The title of the payment can't be empty")
         }
-//        if (payments.paymentAmount == null){
-//            throw InvalidPaymentException("The amount of the payment can't be empty")
-//        }
+        if (payments.paymentAmount.isBlank()){
+            throw InvalidPaymentException("The amount of the payment can't be empty")
+        }
         paymentDao.insertPayments(payments = payments)
     }
 
@@ -34,7 +30,6 @@ class PaymentRepositoryImpl @Inject constructor(
             emit(Resource.Loading())
             paymentDao.getAllPayments().collect {
                 emit(Resource.Success(data = it))
-                Log.d("newpaymentsss", "getAllPayments: $it")
             }
         } catch (e: IOException) {
             emit(Resource.Error("Couldn't reach server. Check your internet connection"))
